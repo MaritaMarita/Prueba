c28eb6e04db2ea9a601892ebcd91698bad65cebe
MaritaMarita <89868671+MaritaMarita@users.noreply.github.com>
2022-04-18 18:57:06 -0300

    Add files via upload
    first commit

---
+++ ejemplo/src/main/java/com/example/ejemplo/model/Student.java
@@ -0,0 +1,33 @@
+package com.example.ejemplo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table
+@Getter @Setter
+
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private String name;
+    private String lastname;
+
+
+    /*
+    @ManyToMany
+    @JoinTable(
+            name = "STUDENTS_SUBJECTS",
+            joinColumns = @JoinColumn(name = "STUDENTS_ID"),
+            inverseJoinColumns = @JoinColumn(name = "SUBJECTS_")
+    )
+     */
+
+
+
+}
